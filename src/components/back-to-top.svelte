8532b4d38810f91f38267c58bfa5e39123a111a0
Brama Udi <bramaudi.al@gmail.com>
2020-06-17 16:11:28 +0700

    Background is white on light theme

--- src/components/back-to-top.svelte
+++ src/components/back-to-top.svelte
@@ -34,8 +34,8 @@
   display: block;
   transition: bottom .5s ease-in-out;
   border-radius: 100%;
-  background: cadetblue;
-  box-shadow: 0 0 8px 2px rgba(0,0,0,0.2);
+  background: #ffffff;
+  box-shadow: 0 0 8px 2px rgba(0,0,0,0.5);
 }
 span {
   display: block;
