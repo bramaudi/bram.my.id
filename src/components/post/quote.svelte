38f3f13f86506db0d2c3fc7aea689c15c0fac9f0
Brama Udi <bramaudi.al@gmail.com>
2020-06-26 19:15:20 +0700

    quote type style adjust

--- src/components/post/quote.svelte
+++ src/components/post/quote.svelte
@@ -6,8 +6,34 @@
   <link rel="stylesheet" href="/css/markdown.css">
 </svelte:head>
 
+<style>
+  .tags {
+    display: block;
+  }
+  .tags a {
+    text-decoration: none;
+    display: inline-block;
+    padding: 0;
+    margin-right: 4px;
+    border-radius: 5px;
+    color: cadetblue;
+  }
+  .author {
+    margin: -1.5rem 0 .5rem;
+    text-align: right;
+  }
+</style>
+
 <div class="quote">
   <div class="date">{post.date}</div>
+
   " <em>{(post.html).replace(/<(.+?)>/g, '')}</em> "
-  <div style="text-align: right">- <strong>{post.author}</strong></div>
+
+  <div class="tags">
+  {#each post.tags as tag}
+    <a href="/blog/tags/{tag}">#{tag}</a>
+  {/each}
+  </div>
+
+  <div class="author">- <strong>{post.author}</strong></div>
 </div>
--- src/posts/quotes/gus-baha-1.md
+++ src/posts/quotes/gus-baha-1.md
@@ -2,7 +2,7 @@
 type: quote
 date: 2020-06-26 06:36:24
 author: Gus Baha
-tags: [hidup]
+tags: [ngaji]
 ---
 
 Tidak shodaqoh, tapi suka jajan itu sudah shodaqoh. Memberi keuntungan penjual makanan 200 perak lebih sopan. Penjualnya seneng dagangannya laris dan tidak tersinggung. Daripada shodaqoh 2000, penerima shodaqoh ada potensi tersinggung.
