7c985405be24b388f07c311f97a3b670a8a0a7de
Brama Udi <bramaudi.al@gmail.com>
2020-07-04 07:09:21 +0700

    Added masonry layout

--- src/components/post-list.svelte
+++ src/components/post-list.svelte
@@ -1,19 +1,49 @@
 <script>
+  import MagicGrid from 'magic-grid';
 	import FullPost from './post/full-post.svelte';
 	import QuickPost from './post/quick-post.svelte';
 	import Quote from './post/quote.svelte';
-  import { theme } from '../stores.js'
+  import { theme } from '../stores.js';
+  import { onMount } from 'svelte';
   export let posts
+
+  onMount(() => {
+    const el = document.querySelector('.grid').children
+    for (let i = 0; i < el.length; i++) {
+      el[i].style.height = (el[i].clientHeight + 20)+'px'
+    }
+
+    execGrid()
+  })
+
+  function execGrid() {
+    if (window.matchMedia( "(min-width: 784px)" ).matches) {
+      let magicGrid = new MagicGrid({
+        container: '.grid',
+        static: true,
+        animate: true,
+        useMin: true,
+        gutter: 10
+      })
+  
+      magicGrid.listen()    
+    }
+  }
 </script>
 
 <style>
-  div {
-    margin-top: 1rem;
+  .grid {
+    padding-top: 1rem;
   }
-  div::after {
-    display: block;
-    content: '';
-    clear: both;
+  .grid > :global(div) {
+    width: 100%;
+  }
+
+  @media (min-width: 784px) {
+    .grid > :global(div) {
+      width: calc((55rem / 2) - 20px);
+      
+    }
   }
 </style>
 
@@ -21,13 +51,15 @@
 	{#if !posts.length}
 		<div class="wrapper">There is no posts found.</div>
 	{/if}
-  {#each posts as post}
-    {#if post.type === 'quote'}
-      <Quote {post} />
-    {:else if post.type === 'post'}
-      <FullPost {post} />
-    {:else if post.type === 'quick'}
-      <QuickPost {post} />
-    {/if}
-  {/each}
+  <div class="grid">
+    {#each posts as post}
+      {#if post.type === 'quote'}
+        <Quote {post} />
+      {:else if post.type === 'post'}
+        <FullPost {post} />
+      {:else if post.type === 'quick'}
+        <QuickPost {post} />
+      {/if}
+    {/each}
+  </div>
 </div>
